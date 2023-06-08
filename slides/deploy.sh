c37bae6b0b8c9fd1b86e8f5db50002394bb556c8
Giannis Koutsou <g.koutsou@cyi.ac.cy>
2023-06-08 16:22:02 +0000

    Made index.html into a link; Separate .html for each day

--- slides/deploy.sh
+++ slides/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cp -v index.html d??.md /srv/http/vhosts/sds402.online/engage/.
+cp -v index.html d??.html d??.md /srv/http/vhosts/sds402.online/engage/.
 cp -v *.png *.jpg /srv/http/vhosts/sds402.online/engage/.
